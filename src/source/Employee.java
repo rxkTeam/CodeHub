9914c0630b796502b7b2c3c0afa2173413bd644e
autumn2014 <283139246@qq.com>
2019-01-10 21:34:20 +0800

    Employee生成属性方法

--- src/source/Employee.java
+++ src/source/Employee.java
@@ -2,4 +2,28 @@ package source;
 
 public class Employee {
     float a,b,c;
+
+    public float getA() {
+        return a;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public float getC() {
+        return c;
+    }
+
+    public void setC(float c) {
+        this.c = c;
+    }
 }
