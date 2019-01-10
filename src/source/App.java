cb7a9d42415c38856384c044606d638149d60ca3
autumn2014 <283139246@qq.com>
2019-01-10 18:02:46 +0800

    Declare main function123456

--- src/source/App.java
+++ src/source/App.java
@@ -5,6 +5,6 @@ package source;
  */
 public class App {
     public static void main(String args[]){
-        System.out.println("hello,world123");
+        System.out.println("hello,world123456");
     }
 }
