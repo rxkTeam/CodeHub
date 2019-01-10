7fb42eba0a3c58a892f3b053feda047b382116b8
autumn2014 <283139246@qq.com>
2019-01-10 17:57:04 +0800

    Declare main function

--- src/source/App.java
+++ src/source/App.java
@@ -4,4 +4,7 @@ package source;
  * Created by Administrator on 2019/1/10/010.
  */
 public class App {
+    public static void main(String args[]){
+        System.out.println("hello,world");
+    }
 }
