79b28d23ed17ff6ea34f2b79b7919e736465f138
autumn2014 <283139246@qq.com>
2019-01-10 18:03:48 +0800

    circle function

--- src/source/App.java
+++ src/source/App.java
@@ -6,5 +6,7 @@ package source;
 public class App {
     public static void main(String args[]){
         System.out.println("hello,world123456");
+        for(int i=0;i<10;i++)
+            System.out.println(i);
     }
 }
